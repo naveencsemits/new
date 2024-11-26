33ffae1a551437c78b75fec132e2e3f90b0522c4
Naveen Chandravanshi <naveen.chandravanshi@crofarm.com>
2024-11-26 11:50:53 +0530

    fix

--- utils/contact.js
+++ utils/contact.js
@@ -1,7 +1,7 @@
 import { openLink } from "./methods"
 
 const contact = {
-    label: `Get in touch`,
+    label: `Get in touch1`,
     heading: `Let’s Work Together`,
     description: `I’m open for new opportunities – especially ambitious or large projects. However, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!.`,
     button: {
